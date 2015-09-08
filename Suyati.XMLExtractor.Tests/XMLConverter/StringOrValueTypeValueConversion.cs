08ca5d616e0b3ff047f425e1b8b196b23ca10d47
skkallayath <skkallayath@suyati.com>
2015-09-08 15:23:42 +0530

    XMLConverter Added  - Few Unit Tests Also added for XMLConverter

---
+++ Suyati.XMLExtractor.Tests/XMLConverter/StringOrValueTypeValueConversion.cs
@@ -0,0 +1,231 @@
+﻿namespace Suyati.XMLExtractor.Tests
+{
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using Suyati.XmlExtractor;
+    using System;
+
+    /// <summary>
+    /// The test class for checking string or value type Value Extraction
+    /// </summary>
+    [TestClass]
+    public class StringOrValueTypeValueConversion
+    {
+        /// <summary>
+        /// The test class for value type extraction
+        /// </summary>
+        private class ValueTypesTestModel
+        {
+            /// <summary>
+            /// The Integer
+            /// </summary>
+            [Element(Name = "int")]
+            public ContentElement<int> Int { get; set; }
+
+            /// <summary>
+            /// The Date
+            /// </summary>
+            [Element(Name = "date")]
+            public ContentElement<DateTime> Date { get; set; }
+
+            /// <summary>
+            /// The Double
+            /// </summary>
+            [Element(Name = "double")]
+            public ContentElement<double> Double { get; set; }
+
+            /// <summary>
+            /// The Float
+            /// </summary>
+            [Element(Name = "float")]
+            public ContentElement<float> Float { get; set; }
+
+            /// <summary>
+            /// The Charactor
+            /// </summary>
+            [Element(Name = "char")]
+            public ContentElement<char> Char { get; set; }
+
+            /// <summary>
+            /// The Boolean
+            /// </summary>
+            [Element(Name = "bool")]
+            public ContentElement<bool> Bool { get; set; }
+        }
+
+        /// <summary>
+        /// The test class for StringOrNullable value type extraction
+        /// </summary>
+        private class StringOrNullableValueTypesTestModel
+        {
+            /// <summary>
+            /// The Integer
+            /// </summary>
+            [Element(Name = "int")]
+            public ContentElement<int?> Int { get; set; }
+
+            /// <summary>
+            /// The Date
+            /// </summary>
+            [Element(Name = "date")]
+            public ContentElement<DateTime?> Date { get; set; }
+
+            /// <summary>
+            /// The Double
+            /// </summary>
+            [Element(Name = "double")]
+            public ContentElement<double?> Double { get; set; }
+
+            /// <summary>
+            /// The Float
+            /// </summary>
+            [Element(Name = "float")]
+            public ContentElement<float?> Float { get; set; }
+
+            /// <summary>
+            /// The Charactor
+            /// </summary>
+            [Element(Name = "char")]
+            public ContentElement<char?> Char { get; set; }
+
+            /// <summary>
+            /// The Boolean
+            /// </summary>
+            [Element(Name = "bool")]
+            public ContentElement<bool?> Bool { get; set; }
+
+            /// <summary>
+            /// The String
+            /// </summary>
+            [Element(Name = "string")]
+            public ContentElement String { get; set; }
+        }
+
+        /// <summary>
+        /// To convert ValueType Elements To XML Node
+        /// </summary>
+        [TestMethod]
+        public void ValueTypeElementsToXML_SuccessResult()
+        {
+            // Creating the model
+            var model = new ValueTypesTestModel()
+            {
+                Bool = new ContentElement<bool>() { Value = true },
+                Char = new ContentElement<char>() { Value = 'c' },
+                Date = new ContentElement<DateTime>() { Value = new DateTime(2015, 10, 10, 12, 13, 15) },
+                Double = new ContentElement<double>() { Value = 123.4 },
+                Float = new ContentElement<float>() { Value = (float)125.6 },
+                Int = new ContentElement<int>() { Value = 1000 }
+            };
+
+            // Extracting Contents
+            var xml = model.ToXml("test");
+
+            // Checking Result
+            Assert.IsNotNull(xml);
+            Assert.IsNotNull(xml.DocumentElement);
+            Assert.AreEqual(xml.DocumentElement["bool"].InnerText, Convert.ToString(true));
+            Assert.AreEqual(xml.DocumentElement["int"].InnerText, Convert.ToString(1000));
+            Assert.AreEqual(xml.DocumentElement["date"].InnerText, Convert.ToString(new DateTime(2015, 10, 10, 12, 13, 15)));
+            Assert.AreEqual(xml.DocumentElement["double"].InnerText, Convert.ToString(123.4));
+            Assert.AreEqual(xml.DocumentElement["float"].InnerText, Convert.ToString((float)125.6));
+            Assert.AreEqual(xml.DocumentElement["char"].InnerText, Convert.ToString('c'));
+        }
+
+        /// <summary>
+        /// To convert ValueType Elements To XML Node
+        /// </summary>
+        [TestMethod]
+        public void ValueTypeElementsToXML_DefaultValueIfNoValuePresent()
+        {
+            // Creating the model
+            var model = new ValueTypesTestModel()
+            {
+                Bool = new ContentElement<bool>(),
+                Char = new ContentElement<char>(),
+                Date = new ContentElement<DateTime>(),
+                Double = new ContentElement<double>(),
+                Float = new ContentElement<float>(),
+                Int = new ContentElement<int>()
+            };
+
+            // Extracting Contents
+            var xml = model.ToXml("test");
+
+            // Checking Result
+            Assert.IsNotNull(xml);
+            Assert.IsNotNull(xml.DocumentElement);
+            Assert.AreEqual(xml.DocumentElement["bool"].InnerText, Convert.ToString(default(bool)));
+            Assert.AreEqual(xml.DocumentElement["int"].InnerText, Convert.ToString(default(int)));
+            Assert.AreEqual(xml.DocumentElement["date"].InnerText, Convert.ToString(default(DateTime)));
+            Assert.AreEqual(xml.DocumentElement["double"].InnerText, Convert.ToString(default(double)));
+            Assert.AreEqual(xml.DocumentElement["float"].InnerText, Convert.ToString(default(float)));
+            Assert.AreEqual(xml.DocumentElement["char"].InnerText, Convert.ToString(default(char)));
+        }
+
+        /// <summary>
+        /// To convert String or Nullable ValueType Elements To XML Node
+        /// </summary>
+        [TestMethod]
+        public void StringOrNullableValueTypeElementsToXML_SuccessResult()
+        {
+            // Creating the model
+            var model = new StringOrNullableValueTypesTestModel()
+            {
+                Bool = new ContentElement<bool?>() { Value = true },
+                Char = new ContentElement<char?>() { Value = 'c' },
+                Date = new ContentElement<DateTime?>() { Value = new DateTime(2015, 10, 10, 12, 13, 15) },
+                Double = new ContentElement<double?>() { Value = 123.4 },
+                Float = new ContentElement<float?>() { Value = (float)125.6 },
+                Int = new ContentElement<int?>() { Value = 1000 },
+                String = new ContentElement() { Value = "string" }
+            };
+
+            // Converting To Xml
+            var xml = model.ToXml("test");
+
+            // Checking Result
+            Assert.IsNotNull(xml);
+            Assert.IsNotNull(xml.DocumentElement);
+            Assert.AreEqual(xml.DocumentElement["bool"].InnerText, Convert.ToString(true));
+            Assert.AreEqual(xml.DocumentElement["int"].InnerText, Convert.ToString(1000));
+            Assert.AreEqual(xml.DocumentElement["date"].InnerText, Convert.ToString(new DateTime(2015, 10, 10, 12, 13, 15)));
+            Assert.AreEqual(xml.DocumentElement["double"].InnerText, Convert.ToString(123.4));
+            Assert.AreEqual(xml.DocumentElement["float"].InnerText, Convert.ToString((float)125.6));
+            Assert.AreEqual(xml.DocumentElement["string"].InnerText, "string");
+            Assert.AreEqual(xml.DocumentElement["char"].InnerText, Convert.ToString('c'));
+        }
+
+        /// <summary>
+        /// To convert String or Nullable ValueType Elements To XML Node
+        /// </summary>
+        [TestMethod]
+        public void StringOrNullableValueTypeElementsToXML_GetNullIfNoValuePresent()
+        {
+            // Creating the model
+            var model = new StringOrNullableValueTypesTestModel()
+            {
+                Bool = new ContentElement<bool?>(),
+                Char = new ContentElement<char?>(),
+                Date = new ContentElement<DateTime?>(),
+                Double = new ContentElement<double?>(),
+                Float = new ContentElement<float?>(),
+                Int = new ContentElement<int?>(),
+                String = new ContentElement()
+            };
+
+            // Converting To Xml
+            var xml = model.ToXml("test");
+
+            // Checking Result
+            Assert.IsNotNull(xml);
+            Assert.IsNotNull(xml.DocumentElement);
+            Assert.AreEqual(xml.DocumentElement["bool"].InnerText, string.Empty);
+            Assert.AreEqual(xml.DocumentElement["int"].InnerText, string.Empty);
+            Assert.AreEqual(xml.DocumentElement["date"].InnerText, string.Empty);
+            Assert.AreEqual(xml.DocumentElement["double"].InnerText, string.Empty);
+            Assert.AreEqual(xml.DocumentElement["float"].InnerText, string.Empty);
+            Assert.AreEqual(xml.DocumentElement["char"].InnerText, string.Empty);
+            Assert.AreEqual(xml.DocumentElement["string"].InnerText, string.Empty);
+        }
+    }
+}
--- XmlExtractor/XMLExtractor.cs
+++ XmlExtractor/XMLExtractor.cs
@@ -39,7 +39,7 @@
                     #region Value Attribute
 
                     // Checking wheather the Value Attribute present
-                    ValueAttribute valueAttribute = GetCustomeAttribute<ValueAttribute>(property);
+                    ValueAttribute valueAttribute = ReflectionHelper.GetCustomeAttribute<ValueAttribute>(property);
 
                     // If present
                     if (valueAttribute != null)
@@ -55,13 +55,13 @@
                     #region Element Attribute
 
                     // Checking wheather there is any Element Attribute present
-                    ElementAttribute elementAttribute = GetCustomeAttribute<ElementAttribute>(property);
+                    ElementAttribute elementAttribute = ReflectionHelper.GetCustomeAttribute<ElementAttribute>(property);
 
                     // If present
                     if (elementAttribute != null)
                     {
                         // Getting the name of node 
-                        var name = GetName(elementAttribute, property);
+                        var name = ReflectionHelper.GetName(elementAttribute, property);
 
                         // Extracting Elemnt from the Node
                         ExtractElementFromNode(item, xmlNode, name, property);
@@ -74,11 +74,11 @@
                     #region Property Attribute
 
                     // Getting the property attribute
-                    PropertyAttribute propertyAttribute = GetCustomeAttribute<PropertyAttribute>(property);
+                    PropertyAttribute propertyAttribute = ReflectionHelper.GetCustomeAttribute<PropertyAttribute>(property);
                     if (propertyAttribute != null)
                     {
                         // Getting the name of property
-                        var name = GetName(propertyAttribute, property);
+                        var name = ReflectionHelper.GetName(propertyAttribute, property);
 
                         // Extract propertry from Node
                         ExtractPropertyFromNode(item, xmlNode, name, property);
@@ -119,7 +119,7 @@
         private static void ExtractPropertyFromNode<T>(T item, XmlNode xmlNode, string name, PropertyInfo property) where T : class
         {
             var propertyType = property.PropertyType;
-            if (IsStringOrValueType(propertyType))
+            if (ReflectionHelper.IsStringOrValueType(propertyType))
             {
                 var attribute = xmlNode.Attributes[name];
                 if (attribute != null)
@@ -140,7 +140,7 @@
         {
             var propertyType = property.PropertyType;
             // Checking whether the type is string or Value Type
-            if (IsStringOrValueType(propertyType))
+            if (ReflectionHelper.IsStringOrValueType(propertyType))
             {
                 // If so, setting the property
                 SetStringOrValueTypeProperty(item, property, xmlNode.InnerText);
@@ -161,7 +161,7 @@
             var propertyType = property.PropertyType;
 
             // Checking wheather it is of Generic List Type (IList<T>/List<T>)
-            if (IsGenericListType(propertyType))
+            if (ReflectionHelper.IsGenericListType(propertyType))
             {
                 // Getting the generic arguments
                 var genericArguments = propertyType.GetGenericArguments();
@@ -188,7 +188,7 @@
                 }
 
             }
-            else if (IsStringOrValueType(propertyType))
+            else if (ReflectionHelper.IsStringOrValueType(propertyType))
             {
                 var nestedNode = xmlNode[name];
                 if (nestedNode != null)
@@ -225,7 +225,7 @@
                 if (node.Name == name)
                 {
                     // If premitive data type
-                    if (IsStringOrValueType(type))
+                    if (ReflectionHelper.IsStringOrValueType(type))
                     {
                         // Getting the value from inner Text
                         var content = GetStringOrValueTypeSafeValue(type, node.InnerText);
@@ -250,53 +250,6 @@
                 // Iterating to the next Sibling
                 node = node.NextSibling;
             }
-        }
-
-        /// <summary>
-        /// To check wheather it is of generic list type
-        /// </summary>
-        /// <param name="type"></param>
-        /// <returns></returns>
-        private static bool IsGenericListType(Type type)
-        {
-            return type.IsGenericType && (type.GetGenericTypeDefinition() == typeof(IList<>) || type.GetGenericTypeDefinition() == typeof(List<>));
-        }
-
-        /// <summary>
-        /// To get name of a attribute
-        /// </summary>
-        /// <param name="attribute"></param>
-        /// <param name="property"></param>
-        /// <returns></returns>
-        private static string GetName(NamedAttribute attribute, PropertyInfo property)
-        {
-            var name = attribute.Name;
-            if (string.IsNullOrEmpty(name))
-            {
-                name = property.Name;
-            }
-            return name;
-        }
-
-        /// <summary>
-        /// To get a custom attribute
-        /// </summary>
-        /// <typeparam name="T"></typeparam>
-        /// <param name="property"></param>
-        /// <returns></returns>
-        private static T GetCustomeAttribute<T>(PropertyInfo property) where T : Attribute
-        {
-            return property.GetCustomAttributes(true).FirstOrDefault(att => att.GetType() == typeof(T)) as T;
-        }
-
-        /// <summary>
-        /// Is string or value type
-        /// </summary>
-        /// <param name="type"></param>
-        /// <returns></returns>
-        private static bool IsStringOrValueType(Type type)
-        {
-            return type.IsValueType || type == typeof(string);
         }
 
         /// <summary>
@@ -340,14 +293,14 @@
                     return Convert.ChangeType(value, type);
                 }
                 // Exception may arise if the date format ends with Timezone abbreviations (like EST, IST, etc..)
-                catch (FormatException e)
+                catch (FormatException)
                 {
                     bool success = false;
 
                     // Replacing the timezone off set in defautl format
                     value = TimezoneHelper.ReplaceOffSetToDefaultFormat(value, out success);
 
-                    if(!success)
+                    if (!success)
                     {
                         // Replacing the abbreviation with corresponding Timezone Offset
                         value = TimezoneHelper.ReplaceTimezoneAbbreviation((string)value);
